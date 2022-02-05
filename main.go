4163b1d8c8b18af17ce7608d2a658c99ddf869c4
oogab <oogab@naver.com>
2022-02-05 16:09:21 +0900

    Marshal and Field Tags

--- main.go
+++ main.go
@@ -1,7 +1,45 @@
 package main
 
-import explorer "github.com/oogab/wookchain/explorer/templates"
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+)
+
+const port string = ":4000"
+
+type URLDescription struct {
+	URL         string `json:"url"`
+	Method      string `json:"method"`
+	Description string `json:"description"`
+	Payload     string `json:"payload,omitempty"`
+}
+
+func documentation(rw http.ResponseWriter, r *http.Request) {
+	data := []URLDescription{
+		{
+			URL:         "/",
+			Method:      "GET",
+			Description: "See Documentation",
+		},
+		{
+			URL:         "/blocks/",
+			Method:      "POST",
+			Description: "Add A Block",
+			Payload:     "data:string",
+		},
+	}
+	rw.Header().Add("Content-Type", "application/json")
+	json.NewEncoder(rw).Encode(data)
+	// b, err := json.Marshal(data)
+	// utils.HandleErr(err)
+	// fmt.Fprintf(rw, "%s", b)
+}
 
 func main() {
-	explorer.Start()
+	// explorer.Start()
+	http.HandleFunc("/", documentation)
+	fmt.Printf("Listening on http://localhost%s", port)
+	log.Fatal(http.ListenAndServe(port, nil))
 }
