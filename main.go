55ee3dab94ececc517c00745c8a718451eed91a5
oogab <oogab@naver.com>
2022-02-05 19:10:46 +0900

    REST API: Gorilla Mux

--- main.go
+++ main.go
@@ -1,11 +1,10 @@
 package main
 
 import (
-	explorer "github.com/oogab/wookchain/explorer/templates"
 	"github.com/oogab/wookchain/rest"
 )
 
 func main() {
-	go explorer.Start(3000)
+	// go explorer.Start(3000)
 	rest.Start(4000)
 }
