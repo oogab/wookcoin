fd0e9c3c362135c2c435e14e61f4f95376d0346c
wook <wook@henge.io>
2022-12-10 23:24:50 +0900

    #6 REST API #2 MarshalText

--- main.go
+++ main.go
@@ -9,32 +9,34 @@ import (
 
 const port string = ":4000"
 
-/**
-/
-GET
-See Documentation
+type URL string
 
-/blocks
-POST
-Create a new block
-*/
+// URL이 어떻게 json으로 Marshal 될지를 정할 수 있다.
+func (u URL) MarshalText() ([]byte, error) {
+	url := fmt.Sprintf("http://localhost%s%s", port, u)
+	return []byte(url), nil
+}
 
 type URLDescription struct {
-	URL         string `json:"url"`
+	URL         URL    `json:"url"`
 	Method      string `json:"method"`
 	Description string `json:"description"`
 	Payload     string `json:"payload,omitempty"`
 }
 
+func (u URLDescription) String() string {
+	return "Hello I'm the URL Description"
+}
+
 func documentation(rw http.ResponseWriter, r *http.Request) {
 	data := []URLDescription{
 		{
-			URL:         "/",
+			URL:         URL("/"),
 			Method:      "GET",
 			Description: "See Documentation",
 		},
 		{
-			URL:         "/",
+			URL:         URL("/blocks"),
 			Method:      "POST",
 			Description: "Add A Block",
 			Payload:     "data:string",
